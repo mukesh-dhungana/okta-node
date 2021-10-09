763397acf63b7ca51cdf454d611e338382da8285
Mukesh Dhungana <dhunganamukesh0702@gmail.com>
2021-10-09 19:41:21 +0545

    test

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,41 @@
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
-import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
-import { oktaAuthRequired } from './lib/oktaAuthRequired';
-
-dotenv.config({
-  path: '.env',
-});
+import cors from "cors"
+import express, { Express, Request, Response } from "express";
+import { oktaAuthRequired } from "./lib/oktaAuthRequired";
 
 const app: Express = express();
 
 app.use(cors());
 
-app.get('/api/locked', oktaAuthRequired, (req: Request, res: Response) => {
+app.get("/api/locked", oktaAuthRequired, (req: Request, res: Response) => {
   res.json({
     messages: [
       {
         date: new Date(),
-        text: 'Token Verified',
+        text: "Token Verified",
       },
       {
         date: new Date(new Date().getTime() - 1000 * 60 * 60),
-        text: 'This is api response!',
+        text: "This is api response!",
       },
     ],
   });
 });
 
-app.get('/api/free', (req: Request, res: Response) => {
+app.get("/api/free", (req: Request, res: Response) => {
   res.json({
     messages: [
       {
         date: new Date(),
-        text: 'HAHAHAHAHA YOU THOT YOU COULD LOCK ME UP',
+        text: "HAHAHAHAHA YOU THOT YOU COULD LOCK ME UP",
       },
     ],
   });
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", process.env.PORT, app.settings.env);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    process.env.PORT,
+    app.settings.env
+  );
 });
