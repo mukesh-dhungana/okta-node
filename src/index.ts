ff77e3636cc596c954bd6ae29240880373e94b64
Mukesh Dhungana <dhunganamukesh0702@gmail.com>
2021-10-09 19:16:38 +0545

    fix app crash iisue

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,6 @@ app.get('/api/free', (req: Request, res: Response) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`We LIVE at https://okta-node.herokuapp.com/`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
