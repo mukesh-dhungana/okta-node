0789514d6e77964f845d17128a91c6589e3eb4a4
Mukesh Dhungana <dhunganamukesh0702@gmail.com>
2021-10-09 20:06:13 +0545

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Loaded")
+const http = require("http");
+
+var server=http.createServer(function(req,res){
+    res.writeHead(200);
+    res.end("Hello world");
+});
+server.listen(process.env.PORT || 5000, function () {
+  console.log("listening on *:5000");
+});
